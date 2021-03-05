81e0cc455ba484e1613e6365f0562e05007af393
K-Philleani <964202623@qq.com>
2021-03-06 01:04:55 +0800

    定义常量

--- main.cpp
+++ main.cpp
@@ -2,7 +2,16 @@
 
 using namespace std;
 
+/* *常量的定义方式
+ * 1.#define 宏常量
+ * 2.const修饰的常量
+ */
+#define Day 7
+
 int main() {
     int a = 10;
     cout << "a = " << a << endl;
+    cout << "一周一共有" << Day << "天" << endl;
+    const int MONTH = 12;
+    cout << "一年一共有" << MONTH << "个月" << endl;
 }
