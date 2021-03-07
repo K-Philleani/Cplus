60cd568ee59ff6b01dbb96992757fc1fb87c1259
K-Philleani <964202623@qq.com>
2021-03-08 00:29:53 +0800

    整型

--- main.cpp
+++ main.cpp
@@ -21,4 +21,21 @@ int main() {
     cout << "一周一共有" << Day << "天" << endl;
     const int MONTH = 12;
     cout << "一年一共有" << MONTH << "个月" << endl;
+
+    // 数据类型的存在意义: 给变量分配合适的内存空间
+
+    // 整型
+    // 1.短整型 (-32758 ~ 32767)
+    short num1 = 10;
+    // 2.整型
+    int num2 = 10;
+    // 3.长整型
+    long num3 = 10;
+    // 4.长长整型
+    long long num4 = 10;
+    cout << "num1 = " << num1 << endl;
+    cout << "num2 = " << num2 << endl;
+    cout << "num3 = " << num3 << endl;
+    cout << "num4 = " << num4 << endl;
+
 }
